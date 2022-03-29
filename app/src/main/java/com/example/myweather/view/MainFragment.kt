8f73f557b0efe257b5ea0465b352af27d3705012
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-03-30 00:27:07 +0300

    Урок 2 - начало

---
+++ app/src/main/java/com/example/myweather/view/MainFragment.kt
@@ -0,0 +1,42 @@
+package com.example.myweather.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.myweather.R
+import com.example.myweather.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = Observer<Any> { data -> renderData(data) }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(requireContext(), "data", Toast.LENGTH_SHORT).show()
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
