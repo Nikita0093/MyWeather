500865daaf4f7451024d37fb7b3d99631bb1299b
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-04-02 01:21:57 +0300

    Урок 2 - добавили рандомизатор и обработали вывод текста на экран

--- app/src/main/java/com/example/myweather/view/MainFragment.kt
+++ app/src/main/java/com/example/myweather/view/MainFragment.kt
@@ -1,15 +1,14 @@
 package com.example.myweather.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.myweather.R
 import com.example.myweather.databinding.FragmentMainBinding
+import com.example.myweather.viewmodel.AppState
 import com.example.myweather.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
@@ -31,13 +30,27 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val observer = Observer<Any> { data -> renderData(data) }
+        val observer = Observer<AppState> { data -> renderData(data) }
         viewModel.getData().observe(viewLifecycleOwner, observer)
-        viewModel.getWeather()
+        binding.buttonTest.setOnClickListener(View.OnClickListener { viewModel.getWeather() })
     }
 
-    private fun renderData(data: Any) {
-        Toast.makeText(requireContext(), "data", Toast.LENGTH_SHORT).show()
+    private fun renderData(data: AppState) {
+        when (data) {
+            is AppState.Error -> {
+                binding.textView.visibility = View.VISIBLE
+                binding.loadingFrame.visibility = View.GONE
+                binding.textView.text = "$data"
+            }
+            is AppState.Loading -> {
+                binding.loadingFrame.visibility = View.VISIBLE
+            }
+            is AppState.Success -> {
+                binding.textView.visibility = View.VISIBLE
+                binding.loadingFrame.visibility = View.GONE
+                binding.textView.text = "Success"
+            }
+        }
 
     }
 
--- app/src/main/java/com/example/myweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/myweather/viewmodel/MainViewModel.kt
@@ -5,19 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.lang.Thread.sleep
 
-class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+class MainViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData()) :
+    ViewModel() {
 
 
-    fun getData(): LiveData<Any> {
+    fun getData(): LiveData<AppState> {
         return liveData
     }
 
 
     fun getWeather() {
         Thread {
-            sleep(1000L)
+            liveData.postValue(AppState.Loading)
+            sleep(3000L)
 
-            liveData.postValue(Any())
+            if ((0..10).random() > 5) {
+                liveData.postValue(AppState.Success(Any()))
+            } else {
+                liveData.postValue(AppState.Error(IllegalArgumentException()))
+            }
         }.start()
 
     }
