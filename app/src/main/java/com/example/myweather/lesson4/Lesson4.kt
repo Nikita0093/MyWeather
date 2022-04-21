67190d17d7c802aadb5abdc9d432dc452dfc73a6
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-04-21 18:51:47 +0300

    Урок 4 - протестировали 5 функций - расширений

---
+++ app/src/main/java/com/example/myweather/lesson4/Lesson4.kt
@@ -0,0 +1,56 @@
+package com.example.myweather.lesson4
+
+import android.util.Log
+
+data class Lesson4(var name: String = "Никита", var age: Int = 28)
+
+var lesson4 = Lesson4()
+
+
+fun test1() {
+    Log.d("@@@", "До применения test1 : $lesson4")
+    with(lesson4) {
+        name = "Джон"
+        age = 20
+    }
+    Log.d("@@@", "После применения test1 : $lesson4 \n")
+
+}
+
+fun test2() {
+    Log.d("@@@", "До применения test2 : $lesson4")
+    lesson4.let {
+        it.age = 25
+        it.name = "Миша"
+    }
+    Log.d("@@@", "После применения test2 : $lesson4 \n")
+
+}
+
+fun test3() {
+    Log.d("@@@", "До применения test3 : $lesson4")
+    lesson4.apply {
+        age = 37
+    }
+    Log.d("@@@", "После применения test3 : $lesson4 \n")
+
+}
+
+fun test4() {
+    Log.d("@@@", "До применения test4 : $lesson4")
+    lesson4.also {
+        it.name = "Карлос"
+        it.age = 99
+    }
+    Log.d("@@@", "После применения test4 : $lesson4 \n")
+
+}
+
+fun test5() {
+    Log.d("@@@", "До применения test5 : $lesson4")
+    lesson4.run {
+        age = 200
+    }
+    Log.d("@@@", "После применения test5 : $lesson4 \n")
+
+}
--- app/src/main/java/com/example/myweather/view/weatherlist/MainActivity.kt
+++ app/src/main/java/com/example/myweather/view/weatherlist/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myweather.view.weatherlist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myweather.R
+import com.example.myweather.lesson4.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.mainContainer, WeatherListFragment.newInstance()).commit();
         }
+        scopingFunctionsTest()
+    }
 
+    private fun scopingFunctionsTest() {
+        test1()
+        test2()
+        test3()
+        test4()
+        test5()
     }
 }
